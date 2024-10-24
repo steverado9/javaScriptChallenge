1c44ac77d843d8cc72b992752967d4a279a69017
stephen <isaacsteverado9@gmail.com>
2024-10-25 00:40:08 +0100

    updated the month

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const span = document.querySelector('header .present-year' ) //getting the span element
 const year = new Date().getFullYear(); //How to get year and put it in a variable
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 //Function to change colors
 function changeColor() {
     span.textContent = year; //putting the year as the text content of the span element
@@ -18,12 +19,12 @@ changeColor();
 function dateAndTime() {
     const dateAndTime = document.querySelector('header p');
     let time = new Date();
-    let month = "October";
+    let monthNum = time.getMonth();
     let day = time.getDay();
     let hours = time.getHours().toLocaleString();
     let minutes = time.getMinutes().toLocaleString();
     let seconds = time.getSeconds().toLocaleString();
-    dateAndTime.textContent = `${month} ${day}, ${year} ${hours}:${minutes}:${seconds}`;
+    dateAndTime.textContent = `${month[monthNum]} ${day}, ${year} ${hours}:${minutes}:${seconds}`;
 }
 setInterval(dateAndTime, 1000);
 ;
